e31bff1706ab672a972d59b07d86c3b570ea1a99
Khoa Pham <khoapbt@rikkeisoft.com>
2018-03-23 11:43:15 +0700

    update

--- server/app.js
+++ server/app.js
@@ -1,8 +1,14 @@
 import express from 'express';
+import path from 'path';
 
 var app = express();
 
 app.set('port', (process.env.PORT || 3000));
 
 
+// Site path
+app.use('/', express.static(path.join(__dirname, './../client/build')))
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname, './../client/build', 'index.html'));
+});
 export default app;
--- start-server.js
+++ start-server.js
@@ -1,11 +1,5 @@
 import app from './server/app.js';
 
-// Site path
-app.use('/', express.static(path.join(__dirname, '/client/build')))
-app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname, '/client/build', 'index.html'));
-});
-
 app.listen(app.get('port'), () => {
     console.log(`Find the server at: http://localhost:${app.get('port')}/`); // eslint-disable-line no-console
 });
